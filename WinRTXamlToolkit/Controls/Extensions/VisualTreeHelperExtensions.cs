2256b00d110a1a5ef620fdd37a40380338b08113
yutaka <shigure@refy.net>
2014-08-16 22:45:59 +0900

    Fix visual tree check

--- WinRTXamlToolkit/Controls/Extensions/VisualTreeHelperExtensions.cs
+++ WinRTXamlToolkit/Controls/Extensions/VisualTreeHelperExtensions.cs
@@ -221,6 +221,7 @@ namespace WinRTXamlToolkit.Controls.Extensions
             var popups = VisualTreeHelper.GetOpenPopups(Window.Current)
                 .Where(p => p.Child != null).Select(p=>p.Child);
             var ancestors = dob.GetAncestors().ToArray();
+            if (ancestors.Length == 0) ancestors = new[] {dob};
             return popups.Any(ancestors.Contains) ||
                 (Window.Current.Content != null && ancestors.Contains(Window.Current.Content));
         }
